ef86cfe777f4d65f0314ad19fab37c2fe9589b84
MohamedRagab93 <mohamed.bashier567@gmail.com>
2026-06-30 15:49:33 +0300

    connect vercal with rialway

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = import.meta.env.REACT_APP_API_URL 
+    ? `${import.meta.env.REACT_APP_API_URL}/posts`  
+           : 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
